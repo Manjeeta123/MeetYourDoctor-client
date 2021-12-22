f1b6d9762f04b871dacf6749e303570aca623005
Nisha Sharma <ns455164@gmail.com>
2021-12-22 01:26:09 -0800

    appointment

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -10,7 +10,11 @@ function Appointment(){
                 <div className="col-md-10 col-sm-10 col-12 m-5">
                      <div className="row justify-content-center bg">
                      <div className="col-md-8 col-sm-11 col-12">
+                         <input type="text" name="PatientName" className="form-control" placeholder="Patient Name"/>
                          <input type="text" name="doctorName" className="form-control" placeholder="Doctor Name"/>
+                         <input type="date" name="date" className="form-control" placeholder="Date"/>
+                         <input type="time" name="Time" className="form-control" placeholder="Time"/>
+                         
                     </div>
                     <div className="col-md-3 offset-md-1 col-sm-10 col-12">
                     <button type="button" className=" btn btn-success"> Book an Appointment</button></div>
